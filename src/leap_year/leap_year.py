c1cb93662d97e3da5487139d5d88f4a098f438bc
dakshdholakia <91942649+dakshdholakia@users.noreply.github.com>
2024-09-09 10:59:40 +0530

    more cases

--- src/leap_year/leap_year.py
+++ src/leap_year/leap_year.py
@@ -2,6 +2,7 @@ def leapyr(inp_year: int):
 
     if inp_year % 100 == 0 and inp_year % 400 != 0:
         return "Not a leap year"
+
     elif inp_year % 4 != 0:
         return "Not a leap year"
 
--- test/test_leap_year.py
+++ test/test_leap_year.py
@@ -1,10 +1,10 @@
 from src.leap_year.leap_year import leapyr
 
 def test_leap_year():
-    assert leapyr(2013) == "Not a leap year"
+    assert leapyr(1997) == "Not a leap year"
 
 def test_leap_year_pos():
-    assert leapyr(2016) == "Leap Year"
+    assert leapyr(1996) == "Leap Year"
 
 def test_100_not_400():
-    assert leapyr(2100) == "Not a leap year"
+    assert leapyr(1800) == "Not a leap year"
