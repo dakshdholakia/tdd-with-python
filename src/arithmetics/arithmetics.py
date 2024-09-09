8f778e89c4dbacb423b988ac8546e64a6733c7c5
dakshdholakia <91942649+dakshdholakia@users.noreply.github.com>
2024-09-09 12:53:53 +0530

    ( ( 4 * 5 ) ) case specific

--- src/arithmetics/arithmetics.py
+++ src/arithmetics/arithmetics.py
@@ -1,3 +1,24 @@
 def arithmetics(inp):
-    if inp.count('(') == inp.count(')'):
-        return 0
+    if inp.count('(') != inp.count(')'):
+        return "Invalid record error"
+    else:
+        inp = inp.replace(" ", "") # ((4*5))
+        inp = inp.replace("(", "") # 4*5))
+        inp = inp.replace(")", "") # 4*5
+
+        spl = inp.split('*')
+        num = [int(n) for n in spl]
+
+        res = 1
+        for i in num:
+            res *= i
+        return res
+
+
+# 1. Check parentheses
+# 2. Remove whitespaces, special characters
+# 3. Split input
+# 4. Extract arithmetic operators
+# 5. Extract numbers
+# 6. Function to apply Step 4 on 5
+# 7. Final output
--- test/arithmetics/test_arithmetics.py
+++ test/arithmetics/test_arithmetics.py
@@ -3,3 +3,9 @@ from src.arithmetics.arithmetics import arithmetics
 
 def test_brackets():
     assert arithmetics('((()()))') == 0
+
+def test_invalid():
+    assert arithmetics('())') == "Invalid record error"
+
+def test_exp():
+    assert arithmetics('( ( 4 * 5 )  )') == 20
