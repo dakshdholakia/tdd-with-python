064520edea720ad9db795645608f546b08e9d72e
dakshdholakia <91942649+dakshdholakia@users.noreply.github.com>
2024-09-11 10:17:26 +0530

    handle push

---
+++ src/stack_kata/stack_kata.py
@@ -0,0 +1,5 @@
+
+def push(num: int):
+    p1 = []
+    p1.append(num)
+    return p1
---
+++ test/stack_kata/test_stack_kata.py
@@ -0,0 +1,5 @@
+from src.stack_kata.stack_kata import push
+
+def test_push():
+    p1 = push(4)
+    assert p1[0] == 4
