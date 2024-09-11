88ea7c91df181bdc8d0a1d5e07921b9e6f3b2656
dakshdholakia <91942649+dakshdholakia@users.noreply.github.com>
2024-09-11 10:40:47 +0530

    handle pop

--- src/stack_kata/stack_kata.py
+++ src/stack_kata/stack_kata.py
@@ -1,5 +1,14 @@
-
-def push(num: int):
+class Stack:
     p1 = []
-    p1.append(num)
-    return p1
+
+    def __init__(self, par):
+        self.p1 = par
+
+    def push(self, num: int):
+        self.p1.append(num)
+        return self.p1
+
+    def pop(self):
+        self.p1.pop()
+        return self.p1
+
--- test/stack_kata/test_stack_kata.py
+++ test/stack_kata/test_stack_kata.py
@@ -1,5 +1,15 @@
-from src.stack_kata.stack_kata import push
+from src.stack_kata.stack_kata import Stack
+
 
 def test_push():
-    p1 = push(4)
+    l1 = []             # arrange
+    st = Stack(l1)      # arrange
+    p1 = st.push(4)     # act
     assert p1[0] == 4
+
+def test_pop():
+    l1 = [4]          # arrange
+    st = Stack(l1)    # arrange
+    p1 = st.pop()
+    assert len(p1) == 0
+    assert len(l1) == 0
