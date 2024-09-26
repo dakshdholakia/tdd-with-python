d23ce80f6fc16a0a3775044b78e48d5f6a56d2df
dakshdholakia <91942649+dakshdholakia@users.noreply.github.com>
2024-09-26 11:05:59 +0530

    react + django todo v2

--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*exbr3vq0v_=q%51-nlc+x8&$s^=5lj^d2jkn1bo&xyz6j)w3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
